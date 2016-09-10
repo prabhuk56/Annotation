9ddd0aca2d1e336a5f743f7e6e50b530dba4b40a
prabhuk56 <prabhuk56@gmail.com>
2016-09-10 18:56:49 +0530

    annotation

---
+++ Annotation/src/com/example/annotation/AnnotationDemo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.annotation;
+
+/**
+ *
+ * @author sylvester
+ */
+public class AnnotationDemo {
+    public static void main(String[] args){
+        System.out.println("Annotation Starts..");
+    }
+    
+}
