d28cb2b9f428e4940691ea5c3d79c5bbbe7a3f43
prabhuk56 <prabhuk56@gmail.com>
2016-09-10 14:37:21 +0530

    Annotation

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.prabhu.Annotation;
+
+/**
+ *
+ * @author sylvester
+ */
+public class HelloWorld {
+ public static void main(String arg[]){
+     System.out.println("Hello World");
+ }    
+}
